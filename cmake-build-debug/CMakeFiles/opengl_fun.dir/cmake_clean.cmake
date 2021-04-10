a83b42ee133fd3bba6c32e280d850bb4a6d8d3a1
Stepan Ilyin <norwend@tuta.io>
2021-04-10 14:13:48 +0300

    initial commit

---
+++ cmake-build-debug/CMakeFiles/opengl_fun.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opengl_fun.dir/main.cpp.o"
+  "opengl_fun"
+  "opengl_fun.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opengl_fun.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
