3994f1653bcf08a3f24e2f57c742cf559509ace2
Stepan Ilyin <norwend@tuta.io>
2022-08-14 22:35:01 +0300

    the triangles are now visible!

--- src/main.cpp
+++ src/main.cpp
@@ -42,6 +42,14 @@ static unsigned int create_shader(const std::string& vertex_shader, const std::s
     glLinkProgram(program);
     glValidateProgram(program);
 
+    int success;
+    char log[512];
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    if(!success) {
+	glGetProgramInfoLog(program, 512, NULL, log);
+	std::cout << log << std::endl;
+    }
+
     glDeleteShader(vs);
     glDeleteShader(fs);
 
@@ -64,25 +72,20 @@ struct Triangle {
     Triangle(float vertices[9]) {
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(*(vertices)), vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, 36, vertices, GL_STATIC_DRAW); // TODO: Remove hardcoded "36".
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
 	glBindBuffer(GL_ARRAY_BUFFER, 0); 
 
-	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
 	glBindVertexArray(0);
     }
 
     ~Triangle() {
-	std::cout << "Calling the destructor!" << std::endl;
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
     }
@@ -126,13 +129,13 @@ int main () {
         0.375f, 0.25f, 0.0f
     };
 
-    std::string vsh1 = "#version 330 core\n"
+    std::string vsh = "#version 330 core\n"
 	"layout (location = 0) in vec3 mPos;\n"
 	"void main() {\n"
-	"    gl_Position = vec4(mPos.x, mPos.y, mPos.z, 1.0);\n"
+	"    gl_Position = vec4(mPos.xyz, 1.0f);\n"
 	"}\0";
 
-    std::string fsh1 = "#version 330 core\n"
+    std::string fsh = "#version 330 core\n"
 	"out vec4 color;\n"
 	"void main() {\n"
 	"    color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
@@ -144,11 +147,11 @@ int main () {
 	"    color = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
 	"}\0";
 
-    unsigned int prog = create_shader(vsh1, fsh1);
-    unsigned int prog2 = create_shader(vsh1, fsh2);
+    unsigned int prog = create_shader(vsh, fsh);
+    unsigned int prog2 = create_shader(vsh, fsh2);
     Triangle t1(vert0);
     Triangle t2(vert1);
-    glUseProgram(prog);
+
 
     std::cout << glGetString(GL_VERSION) << std::endl;
     while(!glfwWindowShouldClose(window))
@@ -162,6 +165,7 @@ int main () {
 	glBindVertexArray(t1.VAO);
 	glDrawArrays(GL_TRIANGLES, 0, 3);
 
+	glUseProgram(prog2);
 	glBindVertexArray(t2.VAO);
 	glDrawArrays(GL_TRIANGLES, 0, 3);
 	
