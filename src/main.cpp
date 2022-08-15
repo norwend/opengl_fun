2ce92c8d81160c3fe82583493def7ed4ccdf2cf2
Stepan Ilyin <norwend@tuta.io>
2022-08-15 14:43:27 +0300

    shader class update

--- src/main.cpp
+++ src/main.cpp
@@ -1,62 +1,14 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
-#include <vector>
 #include <string>
 #include <fstream>
-#include <cmath>
+
+#include "shader.hh"
 
 #define W_WIDTH 1280
 #define W_HEIGHT 720
 
-static unsigned int compile_shader(unsigned int type, const std::string& source)
-{
-    unsigned int id = glCreateShader(type);
-    const char* src = source.c_str();
-
-    glShaderSource(id, 1, &src, nullptr);
-    glCompileShader(id);
-
-    int result;
-    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
-    if (result == GL_FALSE)
-    {
-        int length;
-        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
-        char message[length];
-        glGetShaderInfoLog(id, length, &length, message);
-        std::cout << "Shid happend! :DDDDDD\n" << (type == GL_VERTEX_SHADER ? "vertex" : "fragment")
-            << " shader compiling failure\n" << message << std::endl;
-        glDeleteShader(id);
-        return 0;
-    }
-    return id;
-}
-
-static unsigned int create_shader(const std::string& vertex_shader, const std::string& fragment_shader)
-{
-    unsigned int program = glCreateProgram();
-    unsigned int vs = compile_shader(GL_VERTEX_SHADER, vertex_shader);
-    unsigned int fs = compile_shader(GL_FRAGMENT_SHADER, fragment_shader);
-
-    glAttachShader(program, vs);
-    glAttachShader(program, fs);
-    glLinkProgram(program);
-    glValidateProgram(program);
-
-    int success;
-    char log[512];
-    glGetProgramiv(program, GL_LINK_STATUS, &success);
-    if(!success) {
-	glGetProgramInfoLog(program, 512, NULL, log);
-	std::cout << log << std::endl;
-    }
-
-    glDeleteShader(vs);
-    glDeleteShader(fs);
-
-    return program;
-}
 
 void process_input(GLFWwindow* win) {
     if(glfwGetKey(win, GLFW_KEY_Q) == GLFW_PRESS)
@@ -95,16 +47,6 @@ struct Triangle {
     }
 };
 
-std::string read_file(const std::string& path) {
-    std::ifstream input_file(path);
-    if (!input_file.is_open()) {
-	std::cout << "Could not open the file - '"
-		  << path << "'" << std::endl;
-        exit(EXIT_FAILURE);
-    }
-    return std::string((std::istreambuf_iterator<char>(input_file)), std::istreambuf_iterator<char>());
-}
-
 
 int main () {
     glfwInit();
@@ -137,13 +79,8 @@ int main () {
 	0.0f,  0.5f,  0.0f, 0.0f, 0.0f, 1.0f,
     };
 
-    auto vsh = read_file("shaders/test.vert");
-    auto fsh = read_file("shaders/test.frag");
-
-
-    unsigned int prog = create_shader(vsh, fsh);
     Triangle t1(vert0);
-
+    Shader sh("shaders/test.vert", "shaders/test.frag");
 
     std::cout << glGetString(GL_VERSION) << std::endl;
     while(!glfwWindowShouldClose(window))
@@ -153,12 +90,11 @@ int main () {
 	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	glUseProgram(prog);
-
-	float timevalue = glfwGetTime();
-	float value = std::sin(timevalue);
-	int vertcolorlocation = glGetUniformLocation(prog, "ourColor");
-	glUniform4f(vertcolorlocation, (value / 2.0f) + 0.5f, value, 1-value, 1.0f);
+	sh.use();
+	// float timevalue = glfwGetTime();
+	// float value = std::sin(timevalue);
+	// int vertcolorlocation = glGetUniformLocation(prog, "ourColor");
+	// glUniform4f(vertcolorlocation, (value / 2.0f) + 0.5f, value, 1-value, 1.0f);
 	
 	glBindVertexArray(t1.VAO);
 	glDrawArrays(GL_TRIANGLES, 0, 3);
